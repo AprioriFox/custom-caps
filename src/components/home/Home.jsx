0dc4b2bd38576e670ac5df5c103596a14854e5ba
NurDinBuy <nurik161103@gmail.com>
2022-03-23 22:21:09 +0600

    adf

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Catalog } from '../catalog';
 import HomePage from '../home-page';
+import Header from "../header";
+import './home.css'
 
 const Home = () => {
     const caps =  useSelector(({ caps }) => caps.items)
     return (
-        <div>
+        <div className="home">
+            <Header/>
              <Catalog caps={caps} />
             {/*<HomePage />*/}
         </div>
